ecedf55f1061124f6861b36a2eba491cf40188a5
Mohankrishna07-maddila <maddilamohankrishna32@gmail.com>
2026-09-21 23:40:05 +0530

    pushing the code that is multiple messages notification

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker for Rebuilding Us Pop-up Notifications
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if ('focus' in client) {
+          return client.focus();
+        }
+      }
+      if (self.clients.openWindow) {
+        return self.clients.openWindow('./');
+      }
+    })
+  );
+});
